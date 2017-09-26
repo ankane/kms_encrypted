9584218172f67b91acf469e2d7e59e9490e2fdb4
Andrew Kane <acekane1@gmail.com>
2017-09-25 20:08:13 -0700

    Better tests

--- test/kms_encrypted_test.rb
+++ test/kms_encrypted_test.rb
@@ -1,17 +1,25 @@
 require "test_helper"
 
 class KmsEncryptedTest < Minitest::Test
-  def test_works
-    # create
-    user = User.create!(name: "Test", email: "test@example.org", phone: "555-555-5555")
+  def setup
+    User.delete_all
+    create_user
+  end
+
+  def test_create
+    user = create_user
     assert_equal "test@example.org", user.email
     assert_equal "555-555-5555", user.phone
+  end
 
-    # read
+  def test_read
     user = User.last
     assert_equal "test@example.org", user.email
     assert_equal "555-555-5555", user.phone
+  end
 
+  def test_rotate
+    user = User.last
     fields = user.attributes
     user.rotate_kms_key!
 
@@ -19,8 +27,29 @@ class KmsEncryptedTest < Minitest::Test
       assert user.send(attr) != fields[attr], "#{attr} expected to change"
     end
 
-    user = User.last
+    user.reload
     assert_equal "test@example.org", user.email
+  end
+
+  def test_rotate_phone
+    user = User.last
+    fields = user.attributes
+    user.rotate_kms_key_phone!
+
+    %w(encrypted_phone encrypted_phone_iv encrypted_kms_key_phone).each do |attr|
+      assert user.send(attr) != fields[attr], "#{attr} expected to change"
+    end
+
+    user.reload
     assert_equal "555-555-5555", user.phone
+  end
+
+  private
+
+  def create_user
+    # for now
+    ActiveSupport::Deprecation.silence do
+      User.create!(name: "Test", email: "test@example.org", phone: "555-555-5555")
+    end
   end
 end
