b389e8456bdd8fe5ed22ccd93c5d9f0deb7e7f09
Andrew Kane <andrew@chartkick.com>
2018-12-15 04:36:40 -0800

    Added client test [skip ci]

--- test/client_test.rb
+++ test/client_test.rb
@@ -2,8 +2,6 @@ require_relative "test_helper"
 
 class ClientTest < Minitest::Test
   def test_encrypt
-    skip # if ENV["KMS_KEY_ID"] == "insecure-test-key"
-
     client = KmsEncrypted::Client.new
     plaintext = "hello" * 100
     ciphertext = client.encrypt(plaintext)
