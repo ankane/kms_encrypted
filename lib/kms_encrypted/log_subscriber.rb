b636f5013fb80ecd406825237d713465942dde1d
Andrew Kane <andrew@chartkick.com>
2018-12-15 05:46:36 -0800

    Better notifications for string context [skip ci]

--- lib/kms_encrypted/log_subscriber.rb
+++ lib/kms_encrypted/log_subscriber.rb
@@ -6,7 +6,9 @@ module KmsEncrypted
       data_key = event.payload[:data_key]
       name = data_key ? "Decrypt Data Key" : "Decrypt"
       name += " (#{event.duration.round(1)}ms)"
-      debug "  #{color(name, YELLOW, true)}  Context: #{event.payload[:context].inspect}"
+      context = event.payload[:context]
+      context = context.inspect if context.is_a?(Hash)
+      debug "  #{color(name, YELLOW, true)}  Context: #{context}"
     end
 
     def encrypt(event)
@@ -15,7 +17,9 @@ module KmsEncrypted
       data_key = event.payload[:data_key]
       name = data_key ? "Encrypt Data Key" : "Decrypt"
       name += " (#{event.duration.round(1)}ms)"
-      debug "  #{color(name, YELLOW, true)}  Context: #{event.payload[:context].inspect}"
+      context = event.payload[:context]
+      context = context.inspect if context.is_a?(Hash)
+      debug "  #{color(name, YELLOW, true)}  Context: #{context}"
     end
   end
 end
